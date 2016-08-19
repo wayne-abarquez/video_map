0161fe4a8be66db04bc87524181aa16f446f6287
Your Name <you@example.com>
2016-08-19 18:13:25 +0800

    changed deployment port from 82 to 86

--- conf/gunicorn.conf.py
+++ conf/gunicorn.conf.py
@@ -1,4 +1,4 @@
-bind = '127.0.0.1:8002'
+bind = '127.0.0.1:8006'
 accesslog = '/var/www/video_map/logs/gunicorn-access.log'
 errorlog = '/var/www/video_map/logs/gunicorn-error.log'
 limit_request_line = 0
